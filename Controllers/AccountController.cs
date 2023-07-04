10b7446bcff2520515f4e4539bf82aabe906bc8e
miaca <minhua.cao001@student.weltec.ac.nz>
2023-07-04 19:05:47 +0800

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Antlr.Runtime.Misc;
+using CarSale.Controllers;
+using CarSale.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace CarSale.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Register
+        public ActionResult SignUp() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Seller seller)
+        {
+            using (SellerDbEntities sellerDb = new SellerDbEntities())
+            {
+                //Checks if the entered username already exists in the database. 
+                if (sellerDb.Sellers.Any(x => x.Username == seller.Username))
+                {
+                    //If the username exists,
+                    //sets a error message and returns the "SignUp" view with the provided seller model.
+                    ViewBag.DuplicateMessage = "Username already exist";
+                    return View("SignUp", seller);
+                }
+                //If the username doesn't exist, save the seller to the database.
+                sellerDb.Sellers.Add(seller);
+                sellerDb.SaveChanges();
+
+                // Sets a success message
+                TempData["SuccessMessage"] = "Registration successful";
+            }
+            //clears the ModelState, and redirects the user to the "Login" action of the "Account" controller.     
+            ModelState.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+
+        //Login
+        [HttpGet]
+        public ActionResult Login() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForger yToken]
+        public ActionResult Login(Seller seller)
+        {
+            using (SellerDbEntities sellerDb = new SellerDbEntities())
+            {
+                // Check if the username and password match a seller in the database
+                var sellerDetail = sellerDb.Sellers.Where(x => x.Username == seller.Username && x.Password == seller.Password).FirstOrDefault();
+                if (sellerDetail == null)
+                {
+                    // If no seller is found, set an error message and return the Login view with the snentered seller model
+                    seller.LoginErrorMsg = "Invalid Username or Password";
+                    return View("Login", seller);
+                }
+                else
+                {
+                    // If a seller is found, store the username in the session and
+                    // redirect to the Add action method in the Home controller
+                    Session["Username"] = sellerDetail.Username;
+                    return RedirectToAction("Add", "Home");
+                }
+            }
+        }
+
+        //Logout
+        public ActionResult LogOut()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using CarSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarSale.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Home page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Add Car
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Car car)
+        {
+            using (CarDbEntities carDb = new CarDbEntities())
+            {
+                //add the car detail to database
+                carDb.Cars.Add(car);
+                carDb.SaveChanges();
+                ViewBag.addCarMsg = "Add car successfully!";
+            }
+            return View (car);
+        }
+
+
+        //Car Search
+        public ActionResult CarSearch(string searching)
+        {
+            CarDbEntities carDb = new CarDbEntities();
+            // checks if the search criteria is not empty or null
+            if (!String.IsNullOrEmpty(searching))
+            {
+                //uses the Where method with multiple conditions
+                //to check if any of the car's properties contain the search criteria.
+                //The matching cars are then converted to a list using the ToList method
+                //and stored in the cars variable.
+                var cars = carDb.Cars
+        .Where(x => x.CompanyName.Contains(searching) ||
+                    x.CarModel.Contains(searching) ||
+                    x.Year.Contains(searching) ||
+                    x.Price.ToString().Contains(searching) ||
+                    x.Location.Contains(searching) ||
+                    x.BodyType.Contains(searching))
+        .ToList();
+                //check the user enter value are not matching the car database
+                //it will display the message
+                if (!cars.Any())
+                {
+                    ViewBag.msg = "Not Match!!!";
+                }
+            }
+            //display the car database information
+            return View(carDb.Cars.Where(x => 
+            x.CompanyName.Contains(searching) ||
+            x.CarModel.Contains(searching) || 
+            x.Year.Contains(searching) ||
+            x.Price.ToString().Contains(searching) || 
+            x.Location.Contains(searching) ||
+            x.BodyType.Contains(searching)).ToList());
+        }
+
+
+        //Feedback
+        public ActionResult UFeedback()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UFeedback(Feedback feedback)
+        {
+            using (FeedbackDbEntities feedbackDb = new FeedbackDbEntities())
+            {
+                //add feedback value to the database
+                feedbackDb.Feedbacks.Add(feedback);
+                feedbackDb.SaveChanges();
+                //ViewBag.SuccMsg = "Thank you for your comment!";
+            }
+            
+            ModelState.Clear();
+            return View(feedback);
+        }
+
+
+    }
+}
